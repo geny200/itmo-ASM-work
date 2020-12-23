fdcc8c7ff2da3b41ca75b81d8b4a725169625168
geny200 <Geny200k@mail.ru>
2020-12-23 03:22:04 +0300

    added hw1 (sprintf hex to dec)

--- itmo-ASM-work/hex_print.asm
+++ itmo-ASM-work/hex_print.asm
@@ -1,28 +1,39 @@
 global	_hex_print
 
 section .data
-tmp_str:	resb 32
-tmp_num:	dd 0, 0, 0, 0, 0, 0, 0
+tmp_num:	dd 0, 0, 0, 0, 0, 0, 0, 0
 const_32:	db 32
+sign:		db 0
+filler:		db ' '
+
+section .bss
+tmp_str:	resb 32
 
 section .code
 
 _hex_print:
 	pushad
+	mov		byte [sign], 0
+	mov		byte [filler], ' '
 
 ;read num
 	mov		esi, [esp + 36 + 8]
-	mov		edi, tmp_num
+	;mov		edi, tmp_num
 
-	xor		ecx, ecx
+	xor		eax, eax			;char buffer
+	xor		ecx, ecx			
+
 	xor		edx, edx			;data
 	mov		cl, 8				;count block 4 bits each
-	mov		ebp, 3
-	;mov		ebx, 4				;count block 32 bits each 
+	mov		ebp, 4				;count block 32 bits each 
+
+	cmp		byte [esi], '-'
+	jnz		loop_read
+	inc		esi
 
 loop_read:
-	movzx	eax, byte [esi]
-	test	eax, eax
+	mov		al, byte [esi]
+	test	al, al
 	jz		end_read			;end str
 	inc		esi
 
@@ -59,29 +70,25 @@ continue_read:
 	jnz		loop_read
 
 	mov		cl, 8
-	mov		[edi + ebp * 4], edx			;save data
+	mov		[tmp_num + ebp * 4], edx			;save data
 	xor		edx, edx
 
 	inc		ebp
-	cmp		ebp, 7				;?already read 4 block (16 byte in total)
+	cmp		ebp, 8				;?already read 4 block (16 byte in total)
 	jnz		loop_read
 
 	mov		cl, 0
 	dec		ebp
-	mov		edx, [edi + ebp * 4]
+	mov		edx, [tmp_num + ebp * 4]
 
 end_read:
 	;cl  - shift
 	;ebp - last block
 	;edx - last data
 
-	xor		eax, eax
-	mov		eax, 4
-	mul		cl
-	xchg	al, cl
-
+	shl		cl, 2
 	shl		edx, cl
-	mov		[edi + ebp * 4], edx
+	mov		[tmp_num + ebp * 4], edx
 
 	xor		eax, eax
 	xchg	al, cl
@@ -89,7 +96,7 @@ end_read:
 	xchg	ah, cl
 	sub		ebp, eax
 
-	xor		eax, eax
+
 	mov		esi, ebp
 	
 loop_shrd:
@@ -103,16 +110,47 @@ loop_shrd:
 	cmp		esi, 2
 	jnz		loop_shrd
 	
-convert_to_str:
+;determitating sign
 	;ebp - last block
 
-	xor		ecx, ecx
-	mov		esi, tmp_num
+	mov		edi, tmp_num
 	sub		ebp, 3
 	shl		ebp, 2
-	add		esi, ebp
+	add		edi, ebp
+
+	mov		esi, [esp + 36 + 8]
+	cmp		byte [esi], '-'
+	jnz		left_sign
+
+	not		dword [edi + 12]
+	add		dword [edi + 12], 1
+	not		dword [edi + 8]
+	adc		dword [edi + 8], 0
+	not		dword [edi + 4]
+	adc		dword [edi + 4], 0
+	not		dword [edi]
+	adc		dword [edi], 0
+
+left_sign:
+	test	dword [edi], (1 << 31)
+	jz		convert_to_str
+
+	mov		byte [sign], '-'
+
+	not		dword [edi + 12]
+	add		dword [edi + 12], 1
+	not		dword [edi + 8]
+	adc		dword [edi + 8], 0
+	not		dword [edi + 4]
+	adc		dword [edi + 4], 0
+	not		dword [edi]
+	adc		dword [edi], 0
 	
-	mov		edi, [esp + 36]
+convert_to_str:
+	;esi - first block
+	xor		ecx, ecx
+	xchg	edi, esi
+	mov		edi, tmp_str
 	mov		ebx, 10
 	
 convert_loop:
@@ -138,6 +176,169 @@ convert_div_loop:
 	or		eax, [esi + 12]
 	jnz		convert_loop
 
+read_flag:
+	;ecx - lenght
+	mov		edx, 10
+	xor		eax, eax
+	xor		ebx, ebx				;bh '-'; bl ' ' 
+	xor		ebp, ebp				;ebp - weight
+			
+	mov		esi, [esp + 36 + 4]
+	
+read_flag_loop:
+	mov		al, byte [esi]
+	test	al, al
+	jz		read_flag_end			;end str
+	inc		esi
 
+	cmp		al, '-'
+	jnz		case_2	
+	mov		bh, 1
+	jmp		read_flag_loop
+case_2:
+	cmp		al, '+'
+	jnz		case_3
+	cmp		byte [sign], 0
+	jnz		read_flag_loop
+	mov		byte [sign], '+'
+	jmp		read_flag_loop
+case_3:
+	cmp		al, ' '
+	jnz		case_4
+	mov		bl, 1
+	jmp		read_flag_loop
+case_4:
+	cmp		al, '0'
+	jnz		case_5
+	mov		byte [filler], '0'
+	jmp		read_flag_loop
+
+case_5:
+	cmp		al, '0'
+	jb		read_flag_end			;bad char
+	cmp		al,	'9'
+	ja		read_flag_end			;bad char
+	sub		al, '0'
+	xchg	ebp, eax
+	mul		dl
+	add		ebp, eax				;ebp - weight
+
+	mov		al, byte [esi]
+	test	al, al
+	inc		esi
+	jnz		case_5
+
+
+read_flag_end:
+	;ecx - lenght
+	;ebp - weight
+	;ebx bh '-'; bl ' ' 
+
+	xor		eax, eax
+	sub		ebp, ecx
+
+	mov		esi, tmp_str
+	mov		edi, [esp + 36]
+	cmp		bh, 1
+	jnz		print_right_num
+
+;print_left_num
+	mov		byte [filler], ' '		; bh? filler = ' '
+	call	print_prefix
+	sub		ebp, eax
+	call	print_num
+
+	cmp		ebp, 0
+	jle		exit
+	xchg	ebp, ecx
+	call	print_fill
+	jmp		exit
+
+print_right_num:
+	cmp		byte [filler], '0'
+	jnz		print_right_num_sp
+	call	print_prefix
+	sub		ebp, eax
+
+	cmp		ebp, 0
+	jle		print_right_num_num
+	xchg	ebp, ecx
+	call	print_fill
+	xchg	ebp, ecx
+print_right_num_num:
+	call	print_num
+	jmp		exit
+
+print_right_num_sp:
+	cmp		byte [sign], 0
+	jz		print_right_num_sp_s
+	mov		eax, 1
+print_right_num_sp_s:
+	or		al, bl
+	sub		ebp, eax
+	
+	cmp		ebp, 0
+	jle		print_right_num_sp_prefix
+	xchg	ebp, ecx
+	call	print_fill
+	call	print_prefix
+	xchg	ebp, ecx
+	call	print_num
+	jmp		exit
+
+print_right_num_sp_prefix:
+	call	print_prefix
+	call	print_num
+	jmp		exit
+
+	
+print_num:
+	
+	;ecx - lenght
+	mov		al, byte [esi + ecx - 1] 
+	mov		byte [edi], al
+
+	inc		edi
+	dec		ecx
+	test	ecx, ecx
+	jnz		print_num
+	ret
+
+print_fill:
+	;ecx - lenght	
+	mov		al, byte [filler] 
+print_fill_loop:
+	mov		byte [edi], al
+	inc		edi
+	dec		ecx
+	test	ecx, ecx
+	jnz		print_fill_loop
+	ret
+
+print_prefix:
+	;edi - output
+	;ecx - lenght
+	;ebp - weight
+	;ebx bh '-'; bl ' '
+	xor		eax, eax
+
+	cmp		byte [sign], 0
+	jz		print_sp
+	mov		dl, byte [sign]
+	mov		byte [edi], dl
+	inc		edi
+	inc		eax
+	ret 
+print_sp:
+	test	bl, bl
+	jz		skip_sp
+	mov		byte [edi], ' '
+	inc		edi
+	inc		eax
+skip_sp:
+	ret
+
+
+exit:
 	popad
 	ret
--- itmo-ASM-work/x86_tasks.cpp
+++ itmo-ASM-work/x86_tasks.cpp
@@ -76,12 +76,60 @@ void task_max_sign() {
     printf("number: %d\n my - ms: \t%f\n naive - ms: \t%f", (int)result, (double)(end1 - start), (double)(end2 - end1));
     return;
 }
-
+#include <vector>
+#include <string>
 void task_hec_to_dec() {
-    char buff1[100]{ 0 };
-    hex_print(buff1, buff1, "123456789123456789");
-    std::cout << "out: |" << buff1 << "|";
-    //test_hex(buff1, "", "123456789123456789"); // 562046894926727218533
+    uint32_t nCnt = 0;
+    srand(time(0));
+    std::string sAlfa = "0123456789abcdefABCDEF";
+    std::vector<std::string> pConf = { "+", "-", "0", " ", "9", "+-", "+0", "-0", "09", "017", "0+-9", " +", "- ", "+ ", " 0", "0+- 9", "0+- 1", "0+- 17" };
+    std::vector<std::string> pNums = { "1", "-1", "80000000", "-80000000", "800000000", "-800000000", "1234", "80000000000000008000000000000000", "-80000000000000008000000000000000", "f000c00a0b00f0", "f", "10" };
+    for (auto& iConf : pConf) {
+
+        for (int len = 1; len <= 128 / 4; len++) {
+            for (size_t q = 0; q < 900; q++)
+            {
+                char buff1[200]{ 0 };
+                char buff2[200]{ 0 };
+                std::string sInput = "";
+                int x = rand() % (sAlfa.size() - 1);
+                sInput.push_back(sAlfa[1 + x]);
+                for (int i = 0; i < len - 1; i++) {
+                    x = rand() % sAlfa.size();
+                    sInput.push_back(sAlfa[x]);
+                }
+                hex_print(buff1, iConf.c_str(), sInput.c_str());
+                test_hex(buff2, iConf.c_str(), sInput.c_str());
+
+                if (std::string(buff1) != std::string(buff2)) {
+                    std::cout << std::endl << "Err:" << std::endl
+                        << "conf: |" << iConf << "|" << std::endl
+                        << "numb: |" << sInput << "|" << std::endl
+                        << "my \t|" << buff1 << "|" << std::endl
+                        << "test \t|" << buff2 << "|" << std::endl << std::endl;
+                }
+            }
+
+        }
+        for (auto& iNum : pNums) {
+            //std::cout << ++nCnt << ' ';
+            char buff1[200]{ 0 };
+            char buff2[200]{ 0 };
+            hex_print(buff1, iConf.c_str(), iNum.c_str());
+            test_hex(buff2, iConf.c_str(), iNum.c_str());
+            if (std::string(buff1) != std::string(buff2)) {
+                std::cout << std::endl << "Err:" << std::endl
+                    << "conf: |" << iConf << "|" << std::endl
+                    << "numb: |" << iNum << "|" << std::endl
+                    << "my \t|" << buff1 << "|" << std::endl
+                    << "test \t|" << buff2 << "|" << std::endl << std::endl;
+            }
+        }
+    }
+    
+    char buff1[200]{ 0 };
+    hex_print(buff1, " +", "1234");
+    std::cout << std::endl << "out: |" << buff1 << "|";
 }
 #endif
 
@@ -89,14 +137,14 @@ void x86_tasks() {
 
 #ifdef _M_IX86 
     printf("x86_tasks:\n");
-    printf("arctang:\n");
-    task_arctang();
+    //printf("arctang:\n");
+    //task_arctang();
 
-    printf("\nmax(sign):\n");
-    task_max_sign();
+    //printf("\nmax(sign):\n");
+    //task_max_sign();
 
-    printf("\nmax(unsign):\n");
-    task_max();
+    //printf("\nmax(unsign):\n");
+    //task_max();
 
     printf("\nhec_to_dec:\n");
     task_hec_to_dec();
